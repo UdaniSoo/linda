decb83df6e3211081e38951b14402352bd7abab3
UdaniSoo <isuru.udani1@gmail.com>
2018-10-20 11:25:14 +0800

    Initial commit

---
+++ src/linda/Car.java
@@ -0,0 +1,24 @@
+package linda;
+
+import java.awt.Color;
+
+public class Car {
+
+		// TODO Auto-generated method stub
+		Double avgMilesPG;
+		String license;
+		Color paintColor;
+		Boolean tailingworking;
+		
+		public Car(double inputavgMilesPG, String inputlicense, Color inputpaintColor, Boolean inputtailingWorking)
+		
+		{
+			this.avgMilesPG=inputavgMilesPG;
+			this.license=inputlicense;
+			this.paintColor=inputpaintColor;
+			this.tailingworking=inputtailingWorking;
+			
+		}
+		
+
+}
---
+++ src/linda/carMain.java
@@ -0,0 +1,14 @@
+package linda;
+
+import java.awt.Color;
+
+public class carMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car udaniCar=new Car(25.5, "EN240000", Color.black, true);
+		
+		System.out.println("My Car license:" +udaniCar.license);
+	}
+
+}
