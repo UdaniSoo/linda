421376fee5656cb8afaeb1fcd366f94e9385ab23
UdaniSoo <isuru.udani1@gmail.com>
2018-10-20 23:14:29 +0800

    Learning loops

---
+++ src/linda/Loops.java
@@ -0,0 +1,30 @@
+package linda;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//while
+		/*int x=3;
+		while(x>0)
+		{
+			System.out.println("Current X value :"+x);
+			x=x-1;
+		}
+		System.out.println("Final X value: "+x);
+		*/
+		//do-while
+		int y=3;
+		do
+		{
+			System.out.println("Do y: "+y);
+			y=y-1;
+			
+		}while(y>0);
+		
+		System.out.println("Last y is: "+y);
+
+	}
+
+}
