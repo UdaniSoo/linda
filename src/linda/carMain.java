0a60dbd12bbd5e36f268945598846e1a8d9df82c
ranadewa <ranadewa.thisara@gmail.com>
2018-10-20 11:49:33 +0800

    Example commit

--- src/linda/carMain.java
+++ src/linda/carMain.java
@@ -7,6 +7,8 @@ public class carMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Car udaniCar=new Car(25.5, "EN240000", Color.black, true);
+
+		System.out.println("Pull Request added.");
 	}
 
 }
