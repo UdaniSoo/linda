1c447adc9b7b25c829c815a3db84cf8b26b70566
UdaniSoo <isuru.udani1@gmail.com>
2018-10-20 11:40:16 +0800

    license removed

--- src/linda/carMain.java
+++ src/linda/carMain.java
@@ -7,8 +7,6 @@ public class carMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Car udaniCar=new Car(25.5, "EN240000", Color.black, true);
-		
-		System.out.println("My Car license:" +udaniCar.license);
 	}
 
 }
