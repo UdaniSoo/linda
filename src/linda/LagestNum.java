e7ef6a17804453f9c6f88d23fb9018bf0498cce4
UdaniSoo <isuru.udani1@gmail.com>
2018-10-21 23:08:48 +0800

    Added 2nd smallest number code

--- src/linda/LagestNum.java
+++ src/linda/LagestNum.java
@@ -4,14 +4,20 @@ public class LagestNum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int array[]={5,35,21,90,45,76,34};
+		int array[]={5,35,21,90,1,45,76,34};
 		LagestNum arrayPrint=new LagestNum();
 		//arrayPrint.printArrayTillSize(array);
 		
 		//arrayPrint.printReducingArray(array);
 		arrayPrint.printArray(array);
+		
+		//2nd smallest
+		System.out.println(arrayPrint.getNthSmallestNumber(array, 6));
+		arrayPrint.printNthLargestNumber( array,3);
+		arrayPrint.printArray(array);
 		arrayPrint.sortAscending(array);
 		arrayPrint.printArray(array);
+		
 	
 	}
 	
@@ -41,8 +47,9 @@ public class LagestNum {
 	
 	public void sortAscending(int array[])
 	{
+		System.out.println("Sorting in ascending order");
 		for(int outer=0; outer<array.length; outer++)
-		for(int inner=0; inner<array.length-1; inner++)
+		for(int inner=0; inner<array.length-1-outer; inner++)
 		{
 			if(array[inner]>array[inner+1])
 			{
@@ -63,6 +70,58 @@ public class LagestNum {
 		}
 		System.out.println();
 
+	}
+	
+	public void printNthLargestNumber(int array[],int n)
+	{		
+		for(int t=0; t<n; t++)
+		{
+			for(int innerT=0; innerT<array.length-1-t; innerT++)
+			{
+				
+				if(array[innerT]>array[innerT+1])
+				{
+					int temp=array[innerT+1];
+					array[innerT+1]=array[innerT];
+					array[innerT]=temp;
+					
+				}
+			}
+		}
+		System.out.println("The "+n +" Largest number is "+array[array.length-n]);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getNthSmallestNumber(int array[],int n)
+	{
+		for(int outer=0; outer<n; outer++)
+		{
+		for(int inner=0; inner<array.length-1-outer; inner++)
+		{
+			if(array[inner]<array[inner+1])
+			{
+				int temp=array[inner+1];
+				array[inner+1]=array[inner];
+				array[inner]=temp;
+				
+			}
+		}
+		}
+		return array[array.length-n];
 	}
 
 }
